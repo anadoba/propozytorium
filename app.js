df99508d1ac884f5a6ddebd2e6c6f3b4f6661cc8
Adam Nadoba <anadoba@windowslive.com>
2015-05-04 23:33:17 +0200

    Pusty widok, dodane dodatkowe biblioteki loggera, favicon

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 /* jshint node: true */
-var app = require("express")();
-var httpServer = require("http").Server(app);
-var io = require("socket.io")(httpServer);
-
+var app = require('express')();
+var path = require('path');
+var less = require('less-middleware');
+var httpServer = require('http').Server(app);
+var io = require('socket.io')(httpServer);
+var logger = require('morgan');
+var errorHandler = require('errorhandler');
+var path = require('path');
 var static = require('serve-static');
+
 var port = process.env.PORT || 3000;
+var env = process.env.NODE_ENV || 'development';
+
 
 var oneDay = 86400000;
-
-app.use('/img', static(__dirname + '/public/img', { maxAge: oneDay }));
+app.use('/img', static(__dirname + '/public/img', { 
+    maxAge: oneDay 
+}));
+app.use(less(path.join(__dirname, '/less')));
 app.use('/js/jquery.min.js', static(__dirname + '/bower_components/jquery/dist/jquery.min.js'));
 app.use('/js/jquery.min.map', static(__dirname + '/bower_components/jquery/dist/jquery.min.map'));
 app.use(static(__dirname + '/public'));
+
+if ('development' == env) {
+    app.use(logger('dev'));
+    app.use(errorHandler());
+}
 
 httpServer.listen(port, function () {
     console.log('Aplikacja Propozytorium uruchomiona na porcie ' + port);
