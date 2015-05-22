41eab4474ece0165456697dd8eef5ff428031a3d
Adam Nadoba <anadoba@windowslive.com>
2015-05-22 15:12:11 +0200

    js attached to client

---
+++ public/js/client.js
@@ -0,0 +1,10 @@
+/* jshint browser: true */
+/* globalstrict: true */
+/* devel: true */
+/* global io: false */
+/* global $: false */
+"use strict";
+
+$(document).ready(function () {
+    alert('Strona działa!');
+});
